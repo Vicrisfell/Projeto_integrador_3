938cee08b787755f5880de99b6fc9904d6a3dd3e
tbgbarros <tbg.barros@hotmail.com>
2023-11-25 11:58:48 -0300

    implementacao front com back

--- CODIFICAÇÃO/pi/core/models.py
+++
@@ -1,8 +0,0 @@
-from djongo import models
-
-class Modelo(models.Model):
-    campo1 = models.CharField(max_length=100)
-    campo2 = models.IntegerField()
-
-    def __str__(self) -> str:
-        return self.campo1
